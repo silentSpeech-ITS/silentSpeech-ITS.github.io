ce7835b87031f28dc05b04c9dca5133d1b11110f
Soxa <sora@soxa.dev>
2024-04-11 23:54:33 +0200

    Inserimento domande intervista

---
+++ interviews_qa/questions.js
@@ -0,0 +1,52 @@
+/* global bootstrap: false */
+
+(function () {
+  'use strict'
+
+
+  // Disable empty links and submit buttons
+  document.querySelectorAll('[href="#"], [type="submit"]')
+    .forEach(function (link) {
+      link.addEventListener('click', function (event) {
+        event.preventDefault()
+      })
+    })
+
+  function setActiveItem() {
+    var hash = window.location.hash
+
+    if (hash === '') {
+      return
+    }
+
+    var link = document.querySelector('.bd-aside a[href="' + hash + '"]')
+
+    if (!link) {
+      return
+    }
+
+    var active = document.querySelector('.bd-aside .active')
+    var parent = link.parentNode.parentNode.previousElementSibling
+
+    link.classList.add('active')
+
+    if (parent.classList.contains('collapsed')) {
+      parent.click()
+    }
+
+    if (!active) {
+      return
+    }
+
+    var expanded = active.parentNode.parentNode.previousElementSibling
+
+    active.classList.remove('active')
+
+    if (expanded && parent !== expanded) {
+      expanded.click()
+    }
+  }
+
+  setActiveItem()
+  window.addEventListener('hashchange', setActiveItem)
+})()
